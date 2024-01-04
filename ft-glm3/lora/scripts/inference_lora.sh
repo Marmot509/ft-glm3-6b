a485567510bc39db5f3867fde89ab723e01897c4
Xin <xin@XindeMac-mini.local>
2024-01-04 09:34:51 +0800

    fix bugs

--- ft-glm3/lora/scripts/inference_lora.sh
+++ ft-glm3/lora/scripts/inference_lora.sh
@@ -10,9 +10,9 @@ LORA_PATH=output/lora-lyrics-20231229-062839-1e-4/checkpoint-200/
 
 
 python inference.py \
-    --lora_rank $LORA_RANK \
-    --lora_alpha $LORA_ALPHA \
-    --lora_dropout $LORA_DROPOUT \
-    --lora_path $LORA_PATH \
+    --lora-rank $LORA_RANK \
+    --lora-alpha $LORA_ALPHA \
+    --lora-dropout $LORA_DROPOUT \
+    --lora-path $LORA_PATH \
     --max-new-tokens $MAX_TOKENS 
 
