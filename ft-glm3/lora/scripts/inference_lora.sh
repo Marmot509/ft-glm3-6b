9468b24f65260f42bea26b3bc779a8ce28ea2689
Xin <xliu.thu@gmail.com>
2024-01-05 03:19:09 +0000

    fix config.json issue

--- ft-glm3/lora/scripts/inference_lora.sh
+++ ft-glm3/lora/scripts/inference_lora.sh
@@ -5,8 +5,10 @@ set -ex
 LORA_RANK=8
 LORA_ALPHA=16
 LORA_DROPOUT=0.1
-MAX_TOKENS=1024
-LORA_PATH=output/lora-lyrics-20240103-072747-1e-4/checkpoint-1600/
+MAX_TOKENS=512
+LORA_PATH=output/lora-lyrics-20240103-072747-1e-4/checkpoint-1600/pytorch_model.bin
+MODEL=THUDM/chatglm3-6b-base
+TOKENIZER=THUDM/chatglm3-6b-base
 
 
 
@@ -17,4 +19,6 @@ python inference.py \
     --lora-dropout $LORA_DROPOUT \
     --lora-path $LORA_PATH \
     --max-new-tokens $MAX_TOKENS 
+    --model $MODEL
+    --tokenizer $TOKENIZER
 
